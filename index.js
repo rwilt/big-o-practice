5cce01669242523d4fb0d0dd4eebf7ea7831fbc9
rwilt <rosie.wilt@gmail.com>
2020-09-08 15:12:15 -0400

    add problem

--- index.js
+++ index.js
@@ -30,3 +30,60 @@ println("Found prime at index " + result);
 Program.assertEqual(doSearch(primes, 73), 20);
 Program.assertEqual(doSearch(primes, 79), 21);
 Program.assertEqual(doSearch(primes, 83), 22);
+
+
+// *** ---- IMPLEMENTING SWAP ---- ***  \\ 
+
+
+var swap = function(array, firstIndex, secondIndex) {
+    var temp = array[firstIndex];
+     array[firstIndex] = array[secondIndex];
+     //[9,9,4]
+     array[secondIndex] = temp;
+     //[9,9,4] 
+ };
+ 
+ var testArray = [7, 9, 4];
+ swap(testArray, 0, 1);
+ 
+ println(testArray);
+ 
+ Program.assertEqual(testArray, [9, 7, 4]);
+ 
+ swap(testArray, 2, 0 );
+ Program.assertEqual(testArray, [4,7,9]);
+ swap(testArray, 1,0 );
+ Program.assertEqual(testArray, [7,4,9]);
+ 
+ 
+ // **** ---- FIND INDEX OF MIN VAL IN SUB ARRAY ----- *** \\
+ var indexOfMinimum = function(array, startIndex) {
+    // Set initial values for minValue and minIndex,
+    // based on the leftmost entry in the subarray:  
+    var minValue = array[startIndex];
+    var minIndex = startIndex;
+
+    for (var i = minIndex + 1; i < array.length; i++){
+        //if the value of the number at index i is less than the min value, reset the minimum index to i, and the minimum value to array[i]
+        if (array[i] < minValue){
+         minIndex =  i;
+         minValue = array[i];
+            
+        }
+        
+    }
+    // Loop over items starting with startIndex, 
+    // updating minValue and minIndex as needed:
+    
+    return minIndex;
+}; 
+
+var array = [18, 6, 66, 44, 9, 22, 14];   
+var index = indexOfMinimum(array, 2);
+
+//  For the test array [18, 6, 66, 44, 9, 22, 14], 
+//  the value 9 is the smallest of [..66, 44, 9, 22, 14]
+//  Since 9 is at index 4 in the original array, 
+//  "index" has value 4
+println("The index of the minimum value of the subarray starting at index 2 is " + index + "."  );
+Program.assertEqual(index, 4);
